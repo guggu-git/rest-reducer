8cf221d115ddd69375f5ad82fbf65994a3cdda42
Anshul Nema <anshul.nema@cuemath.com>
2020-11-23 18:24:48 +0530

    generate sourcemaps with buildwatch

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,31 @@
-import resolve from "rollup-plugin-node-resolve";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import babel from "rollup-plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
-import pkg from "./package.json";
+import * as path from 'path';
+import resolve from 'rollup-plugin-node-resolve';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import pkg from './package.json';
+
+const generateSourceMaps = process.env.GENERATE_MAPS === 'true';
 
 export default {
-  input: "lib/index.js",
+  input: 'lib/index.js',
   output: {
     file: pkg.module,
-    exports: "named",
-    format: "cjs"
+    format: 'cjs',
+    exports: 'named',
+    sourcemap: generateSourceMaps,
+    sourcemapPathTransform: relativeSourcePath => {
+      return path.resolve('distribution', relativeSourcePath);
+    },
   },
   plugins: [
     peerDepsExternal(),
     resolve(),
     commonjs({
-      include: "node_modules/**"
+      include: 'node_modules/**',
     }),
     babel({
-      exclude: "node_modules/**" // only transpile our source code
-    })
-  ]
+      exclude: 'node_modules/**', // only transpile our source code
+    }),
+  ],
 };
